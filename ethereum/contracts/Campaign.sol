625ebb9648965153822d9a238be2a6337a9b41f3
Camden Navis <naviscam@gmail.com>
2019-06-16 22:45:03 -0400

    initial commit

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,8 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campainPath = path.resolve(__dirname, 'contracts',)
